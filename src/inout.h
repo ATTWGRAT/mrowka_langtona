ff0c640adfc263c8b8dcea8f7468accbf5a97715
RATATTWG <126884242+ATTWGRAT@users.noreply.github.com>
2023-12-23 16:25:05 +0100

    Update inout.h

--- src/inout.h
+++ src/inout.h
@@ -13,7 +13,7 @@ typedef struct{
     int n; //liczba kolumn
     int i; //liczba iteracji
     char* name; //przedrostek nazwy pliku wyjściowego
-    kierunek kier; //JEDNA Z LITER: L, P, G, D - ODPOWIEDNIO LEWO PRAWO GÓRA DÓŁ. Implementacja za pomocą enuma (argument 'P' odpowiada wartości stałej kierunek.PRAWO w strukturze).
+    kierunek kier; //JEDNA Z LITER: L, P, G, D - ODPOWIEDNIO LEWO PRAWO GÓRA DÓŁ. Implementacja za pomocą enuma (argument 'P' odpowiada wartości stałej PRAWO w strukturze).
     FILE* mapa; //Opcjonalny plik z mapą. W przypadku braku podanego w argumentach ustawić na NULL;
     double procent; //Procentowe wypełnienie mapy. W przypadku nie podania argumentu ustawić na -1;
 }dane_wywolania;
